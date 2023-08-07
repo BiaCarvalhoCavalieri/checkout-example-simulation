2c393a2adc0d005d890c62ebbf732a9a6aed9f1b
BiaCarvalhoCavalieri <bia3003carvalho@gmail.com>
2023-08-07 08:06:55 -0300

    feat: newendpoint

--- web/src/graphql/client.ts
+++ web/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:3333/graphql",
+  uri: "https://mockup-checkout.cyclic.app/graphql",
   cache: new InMemoryCache({
     resultCaching: false,
   }),
